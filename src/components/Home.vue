6ce804861d1ee49706aa3131fb11566f87adff38
claireb25 <claire.bourgeois.cb@gmail.com>
2018-07-11 16:07:34 +0200

    mise en page page accueil, reste fond + media queries

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,18 @@
 <template>
     <div id="home">
-        <div v-bind:key="pres" v-for="pres in presentation"> 
-            <h1>{{pres.nom}}</h1>
-            <h2>Développeuse Web Junior</h2>
-            <p>{{pres.description}}</p>
-            <a class="downloadlink" href="src/assets/Claire_Bourgeois_CV.pdf" download="cv_claire_bourgeois.pdf">Téléchargez mon CV <sup>PDF</sup></a>
+        <div class="contenu-home">
+            <div v-bind:key="pres" v-for="pres in presentation"> 
+                <h1>{{pres.nom}}</h1>
+                <h2>Développeuse Web Junior</h2>
+                <p class="description">{{pres.description}}</p>
+                <a class="downloadlink" href="src/assets/Claire_Bourgeois_CV.pdf" download="cv_claire_bourgeois.pdf">Téléchargez mon CV <sup>PDF</sup></a>
+            </div>
         </div>
-      
         <div class="nav">
-            <router-link class="navlink" to="/Projets">Projets</router-link>
-            <router-link class="navlink" to="/CV">CV</router-link>
-            <router-link class="navlink" to="/Blog">Blog</router-link>
-            <router-link class="navlink" to="/Contact">Contact</router-link>
+            <router-link class="navlink" to="/Projets"><span>Projets</span></router-link>
+            <router-link class="navlink" to="/CV"><span>CV</span></router-link>
+            <router-link class="navlink" to="/Blog"><span>Blog</span></router-link>
+            <router-link class="navlink linklast" to="/Contact"><span>Contact</span></router-link>
         </div>
     </div>    
 </template>
@@ -45,21 +46,61 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .downloadlink{
-        text-decoration: none;
-        text-transform: uppercase;
-        sup{
-            font-size: 10px;
+$bleuclair: #01717D;
+@font-face{
+  font-family: "RobotoReg";
+  src: url("../assets/fonts/Roboto-Regular.ttf")
+}
+#home{
+    font-family: "RobotoReg";
+    .contenu-home{
+        width: 80%;
+        margin: auto;
+        font-size: 14px;
+        h1{
+            margin-bottom: 0;
+            font-size: 20px;
+            
+        }
+        h2{
+            text-transform: uppercase;
+            font-size: 20px;
+            margin: 0;
+        }
+        .description{
+            font-size: 12px;
+        }
+        .downloadlink{
+            text-decoration: none;
+            text-transform: uppercase;
+            color: black;
+            sup{
+                font-size:9px;
+            }
         }
     }
     .nav{
+        width: 100%;
+        height: 50%;
+        position: fixed;
+        bottom:0;
         display: flex;
         flex-direction: column;
+        margin-top: 15px;
         .navlink{
-            height:80px;
+            display: flex;
+            border-top: 1px solid black;
+            height: 25%;
             text-decoration: none;
             text-transform: uppercase;
+            color: black;
+            span{
+                margin: auto 39px ;
+            }
         }
+      
     }
+}
+
     
 </style>
